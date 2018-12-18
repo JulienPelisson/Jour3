a9dd17aa97009ccfc0136da91938f7d57736a4b7
JulienTHP <45801226+JulienTHP@users.noreply.github.com>
2018-12-18 21:44:43 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var affichage = window.setTimeout(popUp, 10000);
+
+
+function popUp(){
+	confirm("Voulez vous voir la sextape des couples Chirac et Macron?");
+		if(true){
+			window.location.href =" https://www.youtube.com/watch?v=dQw4w9WgXcQ ";
+		}else{
+			window.location.href =""
+		}
+	}
